7152fad91a444a4c783775a5a46d1c3837c5005e
ChrisM <medic9r1@gmail.com>
2021-05-14 16:38:55 -0500

    Create BankerRoulette.py

---
+++ Day 4/BankerRoulette.py
@@ -0,0 +1,13 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random
+
+num_of_people = len(names)  #Get the number of names in the list
+
+random_number = random.randint(0, num_of_people - 1)  # Get a random int up between 0 and the num of people
+
+print(names[random_number])
